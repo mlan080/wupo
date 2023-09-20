b10c4f776fb29ca446ea2044548e417891c44ab9
mandy lan <mandy.lan@gmail.com>
2023-09-20 21:19:56 +0200

    change puma config for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "0.0.0.0"
   config.hosts << "https://wupo.fly.dev/"
-  config.port = 3000
+
 end
